01c547d230b42e7240e36ac267efe7183e886e92
heroTM737 <xuantien.nguyen93@gmail.com>
2016-12-07 06:35:39 +0700

    update service

--- controller/game/caro/caroController.js
+++ controller/game/caro/caroController.js
@@ -2,4 +2,20 @@ module.exports = function(app) {
     app.get('/game/caro', function(rep, res) {
         res.send('caro');
     });
+
+    app.post('/game/caro/turn', function(rep, res) {
+        var username = req.body.username;
+        var time = req.body.time;
+        var x = req.body.x;
+        var y = req.body.y;
+
+        if (req.session.username == username) {
+            //check time is not expired
+
+            //save turn
+        } else {
+            res.end('Failed');
+        }
+        
+    });
 };
